3758514a1f51bccaf155dffd25a933f5c7ca384e
Lakshmaji <lakshmajim@users.noreply.github.com>
2022-06-05 15:46:22 +0530

    feat: :sparkles: extend default auth guard
    extending default auth guard functionality. and it works the same way to validate jwt token

--- apps/api/src/user/jwt-auth.guard.ts
+++ apps/api/src/user/jwt-auth.guard.ts
@@ -1,5 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard, IAuthGuard } from '@nestjs/passport';
+import { UserEntity } from './entities/user.entity';
+import UserContext from './models/user.context';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') implements IAuthGuard {
+  public async canActivate(context: ExecutionContext) {
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    await super.canActivate(context);
+
+    const { user }: UserContext = context.switchToHttp().getRequest();
+
+    return user ? true : false;
+  }
+
+  public handleRequest<UserEntity>(err: unknown, user: UserEntity): UserEntity {
+    // You can throw an exception based on either "info" or "err" arguments
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
