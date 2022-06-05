eee831582ee7a030f70c6872bc8d4a2ffe6b05b5
Lakshmaji <lakshmajim@users.noreply.github.com>
2022-06-05 15:53:52 +0530

    feat: :sparkles: public decorator to expose apis to public

--- apps/api/src/user/auth.controller.ts
+++ apps/api/src/user/auth.controller.ts
@@ -6,6 +6,7 @@ import {
   Post,
   UseInterceptors,
 } from '@nestjs/common';
+import { Public } from 'src/public.decorator';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto.ts';
@@ -16,12 +17,14 @@ export class AuthController {
   @Inject(AuthService)
   private readonly authService: AuthService;
 
+  @Public()
   @UseInterceptors(ClassSerializerInterceptor)
   @Post('register')
   private register(@Body() body: RegisterDto): Promise<UserEntity> {
     return this.authService.register(body);
   }
-
+  
+  @Public()
   @UseInterceptors(ClassSerializerInterceptor)
   @Post('login')
   private async login(@Body() body: LoginDto): Promise<{ token: string }> {
--- apps/api/src/user/jwt-auth.guard.ts
+++ apps/api/src/user/jwt-auth.guard.ts
@@ -3,12 +3,27 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard, IAuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from 'src/public.decorator';
 import UserContext from './models/user.context';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') implements IAuthGuard {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
   public async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     // Add your custom authentication logic here
     // for example, call super.logIn(request) to establish a session.
     await super.canActivate(context);
