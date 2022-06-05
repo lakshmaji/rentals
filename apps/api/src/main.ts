064b1ac246f6829451931129402f7d4cd6347c0c
Lakshmaji <lakshmajim@users.noreply.github.com>
2022-06-05 16:16:41 +0530

    feat: :sparkles: add swagger documentation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const docBuild = new DocumentBuilder()
+    .setTitle('Apartments API')
+    .setDescription('Apartments API spec')
+    .setVersion('1.0')
+    .addTag('apartments')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, docBuild);
+  SwaggerModule.setup('docs', app, document);
 
   const config = app.get(ConfigService);
 
