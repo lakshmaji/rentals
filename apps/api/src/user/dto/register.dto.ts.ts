b9409a8637aed80f70e17b2a4953db2b33f3395a
Lakshmaji <lakshmajim@users.noreply.github.com>
2022-06-05 14:59:37 +0530

    feat: :sparkles: Validation for input payload

--- apps/api/src/user/dto/register.dto.ts.ts
+++ apps/api/src/user/dto/register.dto.ts.ts
@@ -1,7 +1,11 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
   public readonly email: string;
 
   public readonly password: string;
 
+  @IsString()
   public readonly name: string;
 }
